24105e379e8467efc614428d7bc41d0a345d39df
Daniel <danielu96@wp.pl>
2024-10-02 15:00:52 +0200

    logo added

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,15 @@
+export default Logo
+import Link from 'next/link';
+import { LuTent } from 'react-icons/lu';
+import { Button } from '../ui/button';
+import { Gi3dHammer } from "react-icons/gi";
+function Logo() {
+    return (
+        <Button size='icon' asChild>
+            <Link href='/'>
+                <Gi3dHammer className='w-20 h-6 px-0.5' />
+                Pick
+            </Link>
+        </Button>
+    );
+}
