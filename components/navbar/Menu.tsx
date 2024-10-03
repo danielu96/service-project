db574433c5124c5f531e8eada32b61166f4965b8
Daniel <danielu96@wp.pl>
2024-10-03 16:24:53 +0200

    change content

--- components/navbar/Menu.tsx
+++ components/navbar/Menu.tsx
@@ -34,23 +34,6 @@ const components: { title: string; href: string; description: string }[] = [
         description:
             "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
     },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
 ]
 
 export function NavigationMenuDemo() {
@@ -58,43 +41,26 @@ export function NavigationMenuDemo() {
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                    <NavigationMenuTrigger>Oferta</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/"
-                                    >
+                        <ul className="grid gap-3 p-4 md:w-[200px] lg:w-[200px]">
 
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            shadcn/ui
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Beautifully designed components that you can copy and
-                                            paste into your apps. Accessible. Customizable. Open
-                                            Source.
-                                        </p>
-                                    </a>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/docs" title="Introduction">
-                                Re-usable components built using Radix UI and Tailwind CSS.
+                            <ListItem href="/plyta" title="Płyta">
+                                Płyta laminowana to podstawowy produkt w produkcji mebli..
                             </ListItem>
-                            <ListItem href="/docs/installation" title="Installation">
+                            <ListItem href="fronty" title="Fronty">
                                 How to install dependencies and structure your app.
                             </ListItem>
-                            <ListItem href="/docs/primitives/typography" title="Typography">
+                            <ListItem href="blaty" title="Blaty">
                                 Styles for headings, paragraphs, lists...etc
                             </ListItem>
                         </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                    <NavigationMenuTrigger>Atualności</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                        <ul className="grid w-[200px] gap-3 p-4 md:w-[200px] md:grid-cols-1 lg:w-[200px] ">
                             {components.map((component) => (
                                 <ListItem
                                     key={component.title}
@@ -110,7 +76,7 @@ export function NavigationMenuDemo() {
                 <NavigationMenuItem>
                     <Link href="/" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Home
+                            O Nas
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
