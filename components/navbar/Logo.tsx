6f49bc5877ed426e4fc935d0a094c4a481c2a6cc
Daniel <danielu96@wp.pl>
2024-10-03 16:30:05 +0200

    icon changed

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -7,9 +7,10 @@ function Logo() {
     return (
         <Button size='icon' asChild>
             <Link href='/'>
-                <Gi3dHammer className='w-20 h-6 px-0.5' />
-                Pick
+                <Gi3dHammer className='w-20 h-6 px-0.8' />
+                <span>Pick</span>
             </Link>
+
         </Button>
     );
 }
