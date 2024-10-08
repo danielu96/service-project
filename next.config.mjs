a700d75fcd147f0a52bb982fcea6dee4e7d61ee6
Daniel <danielu96@wp.pl>
2024-10-08 15:50:23 +0200

    new remotePatterns added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ohzyszlnufwlavkjhyfu.supabase.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'mugstore.pl',
+            },
+            {
+                protocol: 'http',
+                hostname: 'plytex.net.pl'
+            },
+
+        ],
+
+    },
+};
 
 export default nextConfig;
