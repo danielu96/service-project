104cb26b084f2d3da76199d9fd6ca5e09d4f52c5
Daniel <danielu96@wp.pl>
2024-10-09 09:57:48 +0200

    cols added

--- components/home/ProductList.tsx
+++ components/home/ProductList.tsx
@@ -4,7 +4,7 @@ import ProductCard from '../card/ProductCard';
 
 function ProductList({ products }: { products: ProductCardProps[] }) {
     return (
-        <section className='my-5 mx-8'>
+        <section className='mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4'>
             {products.map((product) => {
                 return (
                     <ProductCard key={product.id} product={product}></ProductCard>
