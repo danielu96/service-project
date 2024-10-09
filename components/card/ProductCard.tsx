3281d90aa11181bd8d6977ecc2942e1506f55c8e
Daniel <danielu96@wp.pl>
2024-10-09 10:01:02 +0200

    className added to Images

--- components/card/ProductCard.tsx
+++ components/card/ProductCard.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { ProductCardProps } from '@/utils/types'
+import Image from 'next/image';
 
 const ProductCard = ({ product }: { product: ProductCardProps }) => {
-    const { name, image } = product;
+    const { name, image, header } = product;
     const { id: propertyId, tagline } = product;
     return (
-        <div>
-            {name}- {tagline}
-        </div>
+        <article className='group relative'>
+
+            <div className='relative h-[300px] mb-2 overflow-hidden rounded-md'>
+                <Image
+                    src={image}
+                    fill
+                    sizes='(max-width: 768px) 50vw, (max-width: 1200px) 50vw'
+                    alt={name}
+                    className='rounded-md group-hover:brightness-50 ease-out duration-1000 object-cover transform group-hover:scale-110 transition-transform '
+
+                />
+            </div>
+            <div className='flex justify-between items-center'>
+                <h3 className='text-sm font-semibold mt-1'>
+                    {name}
+                </h3>
+            </div>
+            <p className='text-sm mt-1 text-muted-foreground '>
+                {tagline.substring(0, 40)}
+            </p>
+        </article>
     )
 }
 
