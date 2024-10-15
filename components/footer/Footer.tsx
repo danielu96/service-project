74e97e828cc1d275c85e2e424ae2003c4b388d6d
Daniel <danielu96@wp.pl>
2024-10-15 11:44:28 +0200

    mt added

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,11 +4,11 @@ import { FaFacebookF } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div className="mx-auto w-fit ">
+        <div className="mx-auto mt-5 w-fit ">
             <Link href={"https://www.facebook.com/TwojProfil"}>
                 <FaFacebookF className='mx-auto' size={30} />
             </Link>
-            <div className='text-center mt-2'>
+            <div className='text-center mt-5'>
                 <h4>Odwiedź nas ...</h4>
                 <h6 className='text-center mt-5'>Copyright © 2024 - All right reserved by TW</h6>
             </div>
