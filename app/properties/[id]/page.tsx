65fe46304ea66660e53753d9c76d4af35aa893c9
Daniel <danielu96@wp.pl>
2024-10-22 11:36:53 +0200

    cols-1 added

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -15,10 +15,10 @@ async function DetailsPage({ params }: { params: { id: string } }) {
             <BreadCrumbs name={property.name} />
             <div className='grid md:grid-cols-2 mx-auto my-5 max-w-screen-xl'>
 
-                <div className=' max-w-screen-lg z-auto grid grid-cols-3 space-x-4'>
+                <div className=' max-w-screen-lg z-auto grid grid-cols-1 space-x-4'>
                     <ImageContainer mainImage={property.image} name={property.name} />
-                    <div >  <ImageContainer mainImage={property.image} name={property.name} /></div>
-                    <div >  <ImageContainer mainImage={property.image} name={property.name} /></div>
+                    {/* <div >  <ImageContainer mainImage={property.image} name={property.name} /></div>
+                    <div >  <ImageContainer mainImage={property.image} name={property.name} /></div> */}
                 </div>
                 <div className=' grid-flow-row justify-between z-0   my-auto mx-6'>
                     <h1 className='bagart text-black mt-4 md:-ml-24 md:mt-8 mb-7 text-5xl font-extrabold capitalize '>{property.name}</h1>
