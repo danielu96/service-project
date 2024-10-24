662deec1e47b7f0e4a32bc57a6f9cc199e4d6661
Daniel <danielu96@wp.pl>
2024-10-24 10:10:09 +0200

    invisible added

--- components/navbar/Carousel.tsx
+++ components/navbar/Carousel.tsx
@@ -26,23 +26,47 @@ function CarouselComponent() {
                 >
                     <CarouselContent>
                         <CarouselItem className='bg01 h-96'>
-                            <div className='grid grid-row-2 p-8'>
-                                <h1 className='mx-auto mt-32 p-3 capitalize font-serif font-extrabold text-black bg-slate-100 text-xl md:text-5xl max-w-fit '>This is the company</h1>
+                            <div className='grid grid-row-3 p-8 mx-10 mt-32 sm:mt-24 sm:ml-36 '>
+                                <h1 className='  p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-5xl max-w-fit '>This is the company</h1>
+                                <h3 className='invisible sm:visible p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-2xl max-w-fit border-t-2 border-solid '>what they offer</h3>
+                                <div className='invisible sm:visible flex flex-row gap-4 mt-6 text-xl font-bold '>
+                                    <p className=' bg-primary-foreground rounded-xl py-1 px-3'>board</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>front</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>service</p>
+                                </div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className='bg02 h-96'>
-                            <div className='grid grid-row-2 p-8'>
-                                <h1 className='mx-auto mt-32 p-3 capitalize font-serif font-extrabold text-black bg-slate-100 text-xl md:text-5xl max-w-fit '>What they do ?</h1>
+                            <div className='grid grid-row-3 p-8 mx-10 mt-32 sm:mt-24 sm:ml-36'>
+                                <h1 className=' p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-5xl max-w-fit '>What they do </h1>
+                                <h3 className='invisible sm:visible p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-2xl max-w-fit border-t-2 border-solid '>let me check ...</h3>
+                                <div className='invisible sm:visible flex flex-row gap-4 mt-6 text-xl font-bold'>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>board</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>front</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>service</p>
+                                </div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className='bg03 h-96'>
-                            <div className='grid grid-row-2 p-8'>
-                                <h1 className='mx-auto mt-32 p-3 capitalize font-serif font-extrabold text-black bg-slate-100 text-xl md:text-5xl max-w-fit '>Are they expensive ?</h1>
+                            <div className='grid grid-row-3 p-8 mx-10 mt-32 sm:mt-24 sm:ml-36'>
+                                <h1 className=' p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-5xl max-w-fit '>Are they expensive ?</h1>
+                                <h3 className='invisible sm:visible p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-2xl max-w-fit border-t-2 border-solid '>let me check ...</h3>
+                                <div className='invisible sm:visible flex flex-row gap-4 mt-6 text-xl font-bold'>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>cheap</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>very cheap</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>cheapest in the world</p>
+                                </div>
                             </div>
                         </CarouselItem>
                         <CarouselItem className='bg04 h-96'>
-                            <div className='grid grid-row-2 p-8'>
-                                <h1 className='mx-auto mt-32 p-3 capitalize font-serif font-extrabold text-black bg-slate-100 text-xl md:text-5xl max-w-fit '>rather not ofcourse ?</h1>
+                            <div className='grid grid-row-3 p-8 mx-10 mt-32 sm:mt-24 sm:ml-36'>
+                                <h1 className=' p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-5xl max-w-fit '>Are they good ?</h1>
+                                <h3 className='invisible sm:visible p-3 capitalize font-serif font-extrabold text-black bg-white text-xl md:text-2xl max-w-fit border-t-2 border-solid '>let me check ...</h3>
+                                <div className='invisible sm:visible flex flex-row gap-4 mt-6 text-xl font-bold'>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>good</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>very good</p>
+                                    <p className='bg-primary-foreground rounded-xl py-1 px-3'>best and the better</p>
+                                </div>
                             </div>
                         </CarouselItem>
                     </CarouselContent>
