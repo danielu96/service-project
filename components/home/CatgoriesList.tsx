bad2489812fa2becb81d42e68c2442a6e3c4165b
Daniel <danielu96@wp.pl>
2024-10-24 10:40:23 +0200

    underline added

--- components/home/CatgoriesList.tsx
+++ components/home/CatgoriesList.tsx
@@ -1,5 +1,5 @@
 import { categories } from '@/utils/categories';
-// import { ScrollArea, ScrollBar } from '../ui/scroll-area';
+import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 import Link from 'next/link';
 
 function CategoriesList({
@@ -12,29 +12,29 @@ function CategoriesList({
     const searchTerm = search ? `&search=${search}` : '';
     return (
         <section>
-            {/* <ScrollArea className='py-6'> */}
-            <div className='container flex flex-row gap-x-4'>
-                <div className='ml-6 p-4 items-center cursor-pointer duration-300 hover:text-slate-500 '><Link href='/'>All</Link></div>
+            <ScrollArea className='py-1'>
+                <div className='container flex flex-row text-2xl sm:gap-6'>
+                    <div className='md:ml-8 mt-2 px-2  cursor-pointer duration-300 hover:text-slate-500 rounded-lg'><Link href='/'>All</Link></div>
 
-                {categories.map((item) => {
-                    const isActive = item.label === category;
-                    return (
+                    {categories.map((item) => {
+                        const isActive = item.label === category;
+                        return (
 
-                        <Link
-                            key={item.label}
-                            href={`/?category=${item.label}${searchTerm}`}
-                        >
-                            <article
-                                className={`p-3 flex flex-col items-center cursor-pointer duration-300 hover:text-slate-500  ${isActive ? 'text-rose-600' : ''}`}>
-                                {/* <item.icon className='w-8 h-8' /> */}
-                                <p className='capitalize text-lg mt-1'>{item.label}</p>
-                            </article>
-                        </Link>
-                    );
-                })}
-            </div>
-            {/* <ScrollBar orientation='horizontal' /> */}
-            {/* </ScrollArea> */}
+                            <Link
+                                key={item.label}
+                                href={`/?category=${item.label}${searchTerm}`}
+                            >
+                                <article
+                                    className={` py-1 flex flex-col items-center cursor-pointer duration-300 hover:text-slate-500  ${isActive ? ' font-bold underline' : ''}`}>
+                                    {/* <item.icon className='w-8 h-8' /> */}
+                                    <p className='px-1 capitalize mt-1  rounded-e-xl border-solid border-spacing-1 border-indigo-600'>{item.label}</p>
+                                </article>
+                            </Link>
+                        );
+                    })}
+                </div>
+                <ScrollBar orientation='horizontal' />
+            </ScrollArea>
         </section>
     );
 }
