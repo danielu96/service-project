c31e8f0bb4c95ffc856d8239afc67036427224a0
Daniel <danielu96@wp.pl>
2024-10-25 16:02:01 +0200

    Navsearch

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Logo from './Logo'
 import { NavigationMenuDemo } from './Menu'
-// import Navsearch from './Navsearch'
+import Navsearch from './Navsearch'
 import DarkMode from './DarkMode'
 
 function Navbar() {
@@ -11,7 +11,7 @@ function Navbar() {
             <nav className='navcontainer bg-primary-foreground top-0 z-50 transition-colors hover:text-foreground/80 text-foreground/55 shadow-md'>
                 <div className=' flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-3 '>
                     <Logo />
-                    {/* <Navsearch /> */}
+                    <Navsearch />
                     <div className='flex gap-2'>
                         <DarkMode />
                         <NavigationMenuDemo />
