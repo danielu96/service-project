37675578a162ac218c23eb75d25da4333adb7f90
Daniel <danielu96@wp.pl>
2024-12-10 13:40:36 +0100

    remotePatterns fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'img.clerk.com',
-    //         },
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'ohzyszlnufwlavkjhyfu.supabase.co',
-    //         },
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'mugstore.pl',
-    //         },
-    //         {
-    //             protocol: 'http',
-    //             hostname: 'plytex.net.pl'
-    //         },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'abvxfmkvllpkbhmxefln.supabase.co',
+            }
+        ],
 
-    //     ],
-
-    // },
+    },
 };
 
 
