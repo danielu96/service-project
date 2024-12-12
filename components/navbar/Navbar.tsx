c84c29cf3d713d4367c112fe197b419d3415bd21
Daniel <danielu96@wp.pl>
2024-12-12 12:30:27 +0100

    Navsearch added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,7 @@
-"use client"
 import React from 'react'
 import Logo from './Logo'
-import { NavigationMenuDemo } from './Menu'
-// import Navsearch from './Navsearch'
+import LinksDropdown from './Menu'
+import Navsearch from './Navsearch'
 import DarkMode from './DarkMode'
 
 function Navbar() {
@@ -11,10 +10,10 @@ function Navbar() {
             <nav className='navcontainer bg-primary-foreground top-0 z-50 transition-colors hover:text-foreground/80 text-foreground/55 shadow-md'>
                 <div className=' flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-3 '>
                     <Logo />
-                    {/* <Navsearch /> */}
+                    <Navsearch />
                     <div className='flex gap-2'>
                         <DarkMode />
-                        <NavigationMenuDemo />
+                        <LinksDropdown />
                     </div>
                 </div>
             </nav>
