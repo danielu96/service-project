ecd6836f24a925f054a4f4fd23ec173b22e16487
Daniel <danielu96@wp.pl>
2024-12-17 17:40:29 +0100

    new buttons for favorites

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -3,6 +3,8 @@ import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
 import { LuTrash2, LuPenSquare } from 'react-icons/lu';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 
 type btnSize = 'default' | 'lg' | 'sm';
@@ -61,6 +63,40 @@ export const IconButton = ({ actionType }: { actionType: actionType }) => {
             className='p-2 cursor-pointer'
         >
             {pending ? <ReloadIcon className=' animate-spin' /> : renderIcon()}
+        </Button>
+    );
+};
+export const CardSignInButton = () => {
+    return (
+        <SignInButton mode='modal'>
+            <Button
+                type='button'
+                size='icon'
+                variant='outline'
+                className='p-2 cursor-pointer'
+                asChild
+            >
+                <FaRegHeart />
+            </Button>
+        </SignInButton>
+    );
+};
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+    const { pending } = useFormStatus();
+    return (
+        <Button
+            type='submit'
+            size='icon'
+            variant='outline'
+            className='p-2 cursor-pointer'
+        >
+            {pending ? (
+                <ReloadIcon className='animate-spin' />
+            ) : isFavorite ? (
+                <FaHeart />
+            ) : (
+                <FaRegHeart />
+            )}
         </Button>
     );
 };
