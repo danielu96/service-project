92ec8e00839fb422ceaaf25e975b56a69a662f33
Daniel <danielu96@wp.pl>
2024-12-18 18:40:47 +0100

    DetailsPage fixed

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -5,28 +5,72 @@ import { redirect } from 'next/navigation';
 import Description from '@/components/properties/Description';
 import SubmitReview from '@/components/reviews/SubmitReview';
 import PropertyReviews from '@/components/reviews/PropertyReviews';
-// import { Calendar } from '@/components/ui/calendar';
+import { findExistingReview } from '@/utils/actions';
+import { auth } from '@clerk/nextjs/server';
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import { Separator } from '@radix-ui/react-dropdown-menu';
+import { Calendar } from '@/components/ui/calendar';
+import PropertyRating from '@/components/card/PropertyRating';
 
 async function DetailsPage({ params }: { params: { id: string } }) {
+
   const property = await fetchProposalDetails(params.id);
   if (!property) redirect('/');
+  const { userId } = await auth();
+  const isNotOwner = property.profile?.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
 
   return (
     <section>
       <BreadCrumbs name={property.name} />
-      <section className='blaty h-[300px] md:h-[500px] relative mt-8' >
-        <ImageContainer mainImage={property.image} name={property.name} />
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold capitalize'>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
 
-        <div className=' grid-flow-row z-0  mx-6'>
-          <h1 className=' text-black mt-6  xl:mt-12 mb-7 text-5xl font-extrabold capitalize '>{property.name}</h1>
-          <Description description={property.description} />
-          <p className=' text-pretty text-xs text-end mt-14'>- Welcome -</p>
+
+          <FavoriteToggleButton propertyId={property.id} />
         </div>
-        <SubmitReview propertyId={property.id} />
-        <PropertyReviews propertyId={property.id} />
-        {/* <Calendar /> */}
+      </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
+        <div className='lg:col-span-8'>
+          <div className='flex gap-x-4 items-center'>
+            <h1 className='text-xl font-bold'>{property.name} </h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+
+          <Separator className='mt-4' />
+          <Description description={property.description} />
+
+        </div>
+        <div className='lg:col-span-4 flex flex-col items-center'>
+          <Calendar />
+
+        </div>
       </section>
-    </section >
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+      <PropertyReviews propertyId={property.id} />
+    </section>
+
+
+    // <section>
+    //   <BreadCrumbs name={property.name} />
+    //   <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12' >
+    //     <ImageContainer mainImage={property.image} name={property.name} />
+
+    //     <div className=' grid-flow-row z-0  mx-6'>
+    //       <h1 className=' text-black mt-6  xl:mt-12 mb-7 text-5xl font-extrabold capitalize '>{property.name}</h1>
+    //       <Description description={property.description} />
+    //       <p className=' text-pretty text-xs text-end mt-14'>- Welcome -</p>
+    //     </div>
+    //     <SubmitReview propertyId={property.id} />
+
+    //     {/* <Calendar /> */}
+    //   </section>
+    //   {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+    //   <PropertyReviews propertyId={property.id} />
+    // </section >
 
   );
 }
