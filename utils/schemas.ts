4fa3ab4c25880abce71ab8912320e7ab5a38f963
Daniel <danielu96@wp.pl>
2024-12-18 14:29:23 +0100

    createReviewSchema added

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -68,3 +68,8 @@ export const propertySchema = z.object({
     ),
 
 });
+export const createReviewSchema = z.object({
+    propertyId: z.string(),
+    rating: z.coerce.number().int().min(1).max(5),
+    comment: z.string().min(10).max(1000),
+});
