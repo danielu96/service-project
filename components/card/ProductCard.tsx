72495aa0fba10ce1dc54df0831a25a7214efd86b
Daniel <danielu96@wp.pl>
2024-12-29 17:45:07 +0100

    country , price added

--- components/card/ProductCard.tsx
+++ components/card/ProductCard.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { PropertyCardProps } from '@/utils/types'
+import { ProductCardProps } from '@/utils/types'
 import Image from 'next/image';
 import Link from 'next/link';
 import FavoriteToggleButton from './FavoriteToggleButton';
 import PropertyRating from './PropertyRating';
+import CountryFlagAndName from './CountryFlagAndName';
+import { formatCurrency } from '@/utils/format';
 
-const ProductCard = ({ property }: { property: PropertyCardProps }) => {
-    const { name, image } = property;
-    const { id: propertyId, tagline } = property;
+const ProductCard = ({ property }: { property: ProductCardProps }) => {
+    const { name, image, price } = property;
+    const { country, id: propertyId, tagline } = property;
     return (
         <article className='group relative md:mx-10 '>
             <Link href={`/properties/${propertyId}`}>
@@ -21,18 +23,30 @@ const ProductCard = ({ property }: { property: PropertyCardProps }) => {
                         className=' rounded-md  group-hover:brightness-50 ease-out duration-1000 object-cover transform group-hover:scale-110 transition-transform border-y-4 border-card-foreground'
 
                     />
+
                 </div>
-                <div className='absolute  top-5 right-5 zoom-in-75 bg-primary-foreground rounded-lg px-3 py-2 border-y-2 border-card-foreground'>{name}</div>
+                <div className='absolute  top-5 right-5 zoom-in-75 bg-primary-foreground rounded-lg px-3 py-2 border-y-2 border-card-foreground'>
+                    <p className='text-sm mt-1 font-extrabold'>
+                        <span className='font-semibold'>{formatCurrency(price)} </span>
+
+                    </p>
+                </div>
+
                 <div className='flex justify-between items-center'>
                     <h3 className='capitalize text-sm font-semibold md:text-xl mt-1'>
                         {name}
                     </h3>
+
                     <PropertyRating inPage={false} propertyId={propertyId} />
                 </div>
-                <p className='text-sm mt-1 text-muted-foreground '>
+                <div className='flex justify-between items-center text-sm mt-1 text-muted-foreground '>
                     {tagline.substring(0, 80)}
-                </p>
+                    <CountryFlagAndName countryCode={country} />
+                </div>
+
+
             </Link>
+
             <div className='absolute top-5 left-5 z-1'>
                 <FavoriteToggleButton propertyId={propertyId} />
             </div>
