de2c739c40903a024d0718b993522ad6b617f0fa
Daniel <danielu96@wp.pl>
2024-12-30 17:29:25 +0100

    Stats added

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -13,8 +13,12 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
+import { StatsLoadingContainer } from '@/components/admin/Loading';
+import StatsContainer from '@/components/admin/StatsContainer';
+import { Suspense } from 'react';
 
 async function AdminPage() {
+
     const proposals = await fetchProposals()
     if (proposals.length === 0) {
         return (
@@ -23,47 +27,56 @@ async function AdminPage() {
                 message="create a proposal."
             />
         );
-    }
+    };
+
 
     return (
-        <div>
-            <h5>Proposals: {proposals.length}</h5>
-            <Table>
-                <TableCaption>All Yours Proposals</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Name</TableHead>
-                        {/* <TableHead>Price</TableHead> */}
-                        {/* <TableHead>Nights</TableHead> */}
-                        {/* <TableHead>Total</TableHead> */}
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {proposals.map((proposal) => {
-                        const { id: propertyId, name } = proposal;
-                        // const { totalNightsSum, orderTotalSum } = rental;
-                        return (
-                            <TableRow key={propertyId}>
-                                <TableCell>
-                                    <Link href={`/properties/${propertyId}`}>
-                                        {name}</Link>
-                                </TableCell>
-                                {/* <TableCell>{price}</TableCell>
+        <>
+            <Suspense fallback={<StatsLoadingContainer />}>
+                <StatsContainer />
+            </Suspense>
+            <div >
+
+                <div className='mt-5 ml-2 flex justify-between '>
+                    <h5>Proposals: {proposals.length}</h5>
+                </div>
+                <Table>
+                    <TableCaption>All Yours Proposals</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Name</TableHead>
+                            {/* <TableHead>Price</TableHead> */}
+                            {/* <TableHead>Nights</TableHead> */}
+                            {/* <TableHead>Total</TableHead> */}
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {proposals.map((proposal) => {
+                            const { id: propertyId, name } = proposal;
+                            // const { totalNightsSum, orderTotalSum } = rental;
+                            return (
+                                <TableRow key={propertyId}>
+                                    <TableCell>
+                                        <Link href={`/properties/${propertyId}`}>
+                                            {name}</Link>
+                                    </TableCell>
+                                    {/* <TableCell>{price}</TableCell>
                                 <TableCell>{totalNightsSum}</TableCell>
                                 <TableCell>{orderTotalSum}</TableCell> */}
-                                <TableCell className='flex items-center gap-2'>
-                                    <Link href={`/admin/${propertyId}/edit`}>
-                                        <IconButton actionType='edit'></IconButton>
-                                    </Link>
-                                    <DeleteRental propertyId={propertyId} />
-                                </TableCell>
+                                    <TableCell className='flex items-center gap-2'>
+                                        <Link href={`/admin/${propertyId}/edit`}>
+                                            <IconButton actionType='edit'></IconButton>
+                                        </Link>
+                                        <DeleteRental propertyId={propertyId} />
+                                    </TableCell>
 
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </div>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </div>
+        </>
     );
 }
 function DeleteRental({ propertyId }: { propertyId: string }) {
