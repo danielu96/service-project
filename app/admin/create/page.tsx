75ca4d3419240d61b816aca1c8042a2b415b3723
Daniel <danielu96@wp.pl>
2025-01-08 10:45:16 +0100

    Suspense , LoadingTable added

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -10,40 +10,43 @@ import PriceInput from '@/components/form/PriceInput'
 import CountriesInput from '@/components/form/CountriesInput'
 import CounterInput from '@/components/form/CounterInput'
 import AmenitiesInput from '@/components/form/AmenitiesInput'
-
+import { Suspense } from 'react'
+import LoadingTable from '@/components/booking/LoadingTable'
 
 const page = () => {
     return (
-        <section>
-            <h1 className='text-3xl'>Create</h1>
-            <FormContainer action={createPropertyAction}>
-                <FormInput
-                    name='name'
-                    type='text'
-                    label='Name'
-                    defaultValue='some'
-                />
-                <FormInput
-                    name='tagline'
-                    type='text '
-                    label='Tagline'
-                    defaultValue='about it'
-                />
-                <PriceInput />
-                <CategoriesInput />
-                <CountriesInput />
-                <h3 className='text-lg mt-8 mb-4 font-medium'>Accommodation Details</h3>
-                <CounterInput detail='guests' />
-                <CounterInput detail='bedrooms' />
-                <CounterInput detail='beds' />
-                <CounterInput detail='baths' />
-                <h3 className='text-lg mt-10 mb-6 font-medium'>Amenities</h3>
-                <AmenitiesInput />
-                <TextAreaInput name='description' labelText='Description (10 - 1000 Words)' />
-                <ImageInput />
-                <SubmitButton className='mt-2' text='create property' />
-            </FormContainer>
-        </section>
+        <Suspense fallback={<LoadingTable />}>
+            <section>
+                <h1 className='text-3xl'>Create</h1>
+                <FormContainer action={createPropertyAction}>
+                    <FormInput
+                        name='name'
+                        type='text'
+                        label='Name'
+                        defaultValue='some'
+                    />
+                    <FormInput
+                        name='tagline'
+                        type='text '
+                        label='Tagline'
+                        defaultValue='about it'
+                    />
+                    <PriceInput />
+                    <CategoriesInput />
+                    <CountriesInput />
+                    <h3 className='text-lg mt-8 mb-4 font-medium'>Accommodation Details</h3>
+                    <CounterInput detail='guests' />
+                    <CounterInput detail='bedrooms' />
+                    <CounterInput detail='beds' />
+                    <CounterInput detail='baths' />
+                    <h3 className='text-lg mt-10 mb-6 font-medium'>Amenities</h3>
+                    <AmenitiesInput />
+                    <TextAreaInput name='description' labelText='Description (10 - 1000 Words)' />
+                    <ImageInput />
+                    <SubmitButton className='mt-2' text='create property' />
+                </FormContainer>
+            </section>
+        </Suspense>
     )
 }
 
