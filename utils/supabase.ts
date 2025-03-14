eb4436ca1e9bcdde7fdba22af8f9c7516067c7a6
Daniel <danielu96@wp.pl>
2025-03-14 11:17:18 +0100

    new bucket name

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,18 +1,17 @@
+// 'use client'
+
 import { createClient } from '@supabase/supabase-js';
 
-const bucket = 'temp-service';
+const bucket = 'service';
 
-// Create a single supabase client for interacting with your database
 export const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_KEY as string
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
     const timestamp = Date.now();
-    // const newName = `/users/${timestamp}-${image.name}`;
     const newName = `${timestamp}-${image.name}`;
-
     const { data, error } = await supabase.storage
         .from(bucket)
         .upload(newName, image, {
